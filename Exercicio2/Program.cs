7d5fe5f41a7d1a0ff59d8a78a3cf13e95abaca55
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-09-01 21:52:00 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variavel pessoa
+            dados X;
+            dados Y;
+ //==============================================================================
+            // instanciar variavel
+            X = new dados();
+            Y = new dados();
+ //==============================================================================
+            // informação do primeiro usuario
+            Console.WriteLine("coloque seu nome");
+            X.N = Console.ReadLine();
+            Console.WriteLine("coloque sua idade");
+            X.I = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+ //==============================================================================
+            // informação do segundo usuario
+            Console.WriteLine("coloque o nome da segunda pessoa");
+            Y.N = Console.ReadLine();
+            Console.WriteLine("coloque a idade da segunda pessoa");
+            Y.I = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+ //==============================================================================
+            //parâmetro para ver a pessoa mais velha
+            if (X.I >= Y.I)
+            {
+                Console.WriteLine("PESSOA MAIS VELHA " + X.N);
+            }
+            else
+            {
+                Console.WriteLine("PESSOA MAIS VELHA " + Y.N);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Exercicio2/dados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    class dados
+    {
+        // Variaveis 
+        // N de NOME
+        public string N;
+        // I de Idade
+        public int I;
+
+
+    }
+}
